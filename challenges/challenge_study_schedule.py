bb495f0b03a0c3b9898c605f4545f8b3b2d71786
geiselrocha <geiselrocha@hotmail.com>
2023-02-27 22:45:58 -0300

    feat: implements challenge_study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    try:
+        for entry, exit in permanence_period:
+            if entry <= target_time and exit >= target_time:
+                counter += 1
+    except (Exception):
+        return None
+    return counter
