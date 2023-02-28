a0d8a20f1e067657f3c0b9597d9b8c0a9197384b
geiselrocha <geiselrocha@hotmail.com>
2023-02-27 23:26:32 -0300

    test: implements test_ encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("encryptmessage", "test")
+    assert encrypt_message("encryptmessage", 2) == "egassemtpyrc_ne"
+    assert encrypt_message("encryptmessage", 3) == "cne_egassemtpyr"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(["encryptmessagem"], 4)
+    assert encrypt_message("encryptmessage", 5) == "yrcne_egassemtp"
+    assert encrypt_message("encryptmessage", 2345) == "egassemtpyrcne"
+    
