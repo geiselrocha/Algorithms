416e1ac85b4c436b59bd265a1d2408add480ebeb
geiselrocha <geiselrocha@hotmail.com>
2023-03-07 17:57:25 -0300

    fix: lint error on is_anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,6 +7,7 @@ def merge_sort(string: list):
     right = merge_sort(string[mid:])
     return merge(left, right)
 
+
 def merge(left: list, right: list):
     i, j = 0, 0
     result = []
@@ -21,8 +22,11 @@ def merge(left: list, right: list):
     result += right[j:]
     return result
 
+
 def is_anagram(first_string, second_string):
     first_sort = ''.join(merge_sort(list(first_string.lower())))
     second_sort = ''.join(merge_sort(list(second_string.lower())))
-    is_anagram = (first_sort == second_sort and bool (first_string) and bool (second_string))
+    is_anagram = (first_sort == second_sort
+        and bool (first_string)
+        and bool (second_string))
     return (first_sort, second_sort, is_anagram)
