6e9fd9064d6dbb5d21d16805c9a631a55e3f7917
geiselrocha <geiselrocha@hotmail.com>
2023-03-07 22:32:00 -0300

    feat: implements is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    word = word.lower()
+    reversed_word = word[::-1]
+
+    for n in range(0, len(word)):
+        if word[n] != reversed_word[n]:
+            return False
+
+    return True
+
