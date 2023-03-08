d7c43a29c46dcb24f561747ff55100a678a888a4
geiselrocha <geiselrocha@hotmail.com>
2023-03-07 21:45:47 -0300

    feat: implements challenge_find_the_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+    
+    sorted_nums = sorted(nums)
+    number = 0
+    
+    for n in range(number, len(nums) - 1):
+        if sorted_nums[n] == sorted_nums[n + 1]:
+            number = sorted_nums[n]
+    if not number or number < 1:
+        return False
+
+    return number
